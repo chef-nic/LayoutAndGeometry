30a26c99210879b057e4a26d662281e5afdd0cdc
chef-nic <nicholasajohnson@me.com>
2024-08-06 13:08:41 -0700

    rotating view playground

--- LayoutAndGeometry/ContentView.swift
+++ LayoutAndGeometry/ContentView.swift
@@ -8,14 +8,34 @@
 import SwiftUI
 
 struct ContentView: View {
+    let colors: [Color] = [.red, .green, .blue, .orange, .pink, .purple, .yellow]
+
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+        GeometryReader { fullView in
+            ScrollView(.vertical) {
+                ForEach(0..<50) { index in
+                    GeometryReader { proxy in
+                        Text("Row #\(index)")
+                            .font(.title)
+                            .frame(maxWidth: .infinity)
+                            .background(color(for: proxy.frame(in: .global).minY, and: fullView.size.height))
+                            .rotation3DEffect(.degrees(proxy.frame(in: .global).minY - fullView.size.height / 2) / 5, axis: (x: 0, y: 1, z: 0))
+                            .opacity(proxy.frame(in: .global).maxY / 200)
+                            .scaleEffect(
+                                CGSize(
+                                    width: max(0.5, (proxy.frame(in: .global).minY / fullView.size.height)),
+                                    height: max(0.5, (proxy.frame(in: .global).minY / fullView.size.height))
+                                )
+                            )
+                    }
+                    .frame(height: 40)
+                }
+            }
         }
-        .padding()
+    }
+    
+    func color(for yPos: Double, and viewHeight: Double) -> Color {
+        return Color(hue: min(1, yPos / viewHeight), saturation: 0.8, brightness: 1)
     }
 }
 
